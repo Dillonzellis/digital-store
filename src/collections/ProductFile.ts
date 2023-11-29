94836a88fde1eab4ccaffb8fad36106176d8b044
dillonzellis <dillonzellis@gmail.com>
2023-11-29 14:47:25 -0500

    finish ProductFile collection admin

--- src/collections/ProductFile.ts
+++ src/collections/ProductFile.ts
@@ -24,6 +24,38 @@ const yourOwnAndPurchased: Access = async ({ req }) => {
   });
 
   const ownProductFileIds = products.map((prod) => prod.product_files).flat();
+
+  const { docs: orders } = await req.payload.find({
+    collection: "orders",
+    depth: 2,
+    where: {
+      user: {
+        equals: user.id,
+      },
+    },
+  });
+
+  const purchasedProductFileIds = orders
+    .map((order) => {
+      return order.products.map((product) => {
+        if (typeof product === "string")
+          return req.payload.logger.error(
+            "Search depth not sufficient to find purchased file IDs",
+          );
+
+        return typeof product.product_files === "string"
+          ? product.product_files
+          : product.product_files.id;
+      });
+    })
+    .filter(Boolean)
+    .flat();
+
+  return {
+    id: {
+      in: [...ownProductFileIds, ...purchasedProductFileIds],
+    },
+  };
 };
 
 export const ProductFile: CollectionConfig = {
@@ -36,11 +68,13 @@ export const ProductFile: CollectionConfig = {
   },
   access: {
     read: yourOwnAndPurchased,
+    update: ({ req }) => req.user.role === "admin",
+    delete: ({ req }) => req.user.role === "admin",
   },
   upload: {
     staticURL: "/product_files",
     staticDir: "product_files",
-    mimeTypes: ["image/*", "fonts/", "application/postscript"],
+    mimeTypes: ["image/*", "font/*", "application/postscript"],
   },
   fields: [
     {
