a5d45f7d5c6ef4be565c3471c9f16284a7a52c6a
dillonzellis <dillonzellis@gmail.com>
2023-12-06 19:01:33 -0500

    add addItem method to Cart component

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,14 @@
+"use client";
+
+import { useCart } from "@/hooks/use-cart";
+import { formatPrice } from "@/lib/utils";
 import { ShoppingCart } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import CartItem from "./CartItem";
+import { buttonVariants } from "./ui/button";
+import { ScrollArea } from "./ui/scroll-area";
+import { Separator } from "./ui/separator";
 import {
   Sheet,
   SheetContent,
@@ -7,15 +17,16 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "./ui/sheet";
-import { Separator } from "./ui/separator";
-import { formatPrice } from "@/lib/utils";
-import Link from "next/link";
-import { buttonVariants } from "./ui/button";
-import Image from "next/image";
 
 export const Cart = () => {
-  const itemCount = 0;
+  const { items } = useCart();
+  const itemCount = items.length;
   const fee = 1;
+
+  const cartTotal = items.reduce(
+    (total, { product }) => total + product.price,
+    0,
+  );
 
   return (
     <Sheet>
@@ -25,18 +36,21 @@ export const Cart = () => {
           className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
         />
         <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-          0
+          {itemCount}
         </span>
       </SheetTrigger>
       <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
         <SheetHeader className="space-y-2.5 pr-6">
-          <SheetTitle>Cart (0)</SheetTitle>
+          <SheetTitle>Cart ({itemCount})</SheetTitle>
         </SheetHeader>
         {itemCount > 0 ? (
           <>
             <div className="flex w-full flex-col pr-6">
-              {/* TODO: cart logic */}
-              cart items
+              <ScrollArea>
+                {items.map(({ product }) => (
+                  <CartItem key={product.id} product={product} />
+                ))}
+              </ScrollArea>
             </div>
             <div className="space-y-4 pr-6">
               <Separator />
@@ -53,7 +67,7 @@ export const Cart = () => {
 
                 <div className="flex">
                   <span className="flex-1">Total</span>
-                  <span>{formatPrice(fee)}</span>
+                  <span>{formatPrice(cartTotal + fee)}</span>
                 </div>
               </div>
 
