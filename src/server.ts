1c9152f39386b8c5b328b18660bf1ba13851595e
dillonzellis <dillonzellis@gmail.com>
2023-12-11 10:35:57 -0500

    add middleware for cart page

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import express from "express";
 import { IncomingMessage } from "http";
 import nextBuild from "next/dist/build";
 import path from "path";
+import { PayloadRequest } from "payload/types";
+import { parse } from "url";
 import { getPayloadClient } from "./get-payload";
 import { nextApp, nextHandler } from "./next-utils";
 import { appRouter } from "./trpc";
@@ -42,6 +44,22 @@ const start = async () => {
       },
     },
   });
+
+  const cartRouter = express.Router()
+
+  cartRouter.use(payload.authenticate)
+
+  cartRouter.get('/', (req, res)=>{
+    const request = req as PayloadRequest
+
+    if(!request.user) return res.redirect('/sign-in?origin=cart')
+
+    const parsedUrl = parse(req.url, true)
+
+    return nextApp.render(req, res, '/cart', parsedUrl.query)
+  })
+
+  app.use('/cart', cartRouter)
 
   if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
