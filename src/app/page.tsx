965f42ad8ef21ccf808675558130040328d9f716
dillonzellis <dillonzellis@gmail.com>
2023-12-11 14:13:34 -0500

    change maxwidthwrapper import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
+import  MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import ProductReel from "@/components/ProductReel";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
